c0c2f5dc5224d8058bf8d10df5c2e87b83fe0656
Edex01 <jorasiac@gmail.com>
2020-08-23 19:47:54 +0530

    Major Bugs Fixed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,40 +1,14 @@
-import React,{component} from "react";
+import React,{Component} from "react";
 
-class Button extends component{
+class Button extends Component{
 
     render(){
-
-        // array of buttons
-        cont buttons = [
-            {symbol:'C', cols:3, action: this.reset},
-            {symbol:'/', cols:1, action: this.addToCurrent},
-            {symbol:'7', cols:1, action: this.addToCurrent},
-            {symbol:'8', cols:1, action: this.addToCurrent},
-            {symbol:'9', cols:1, action: this.addToCurrent},
-            {symbol:'x', cols:1, action: this.addToCurrent},
-            {symbol:'4', cols:1, action: this.addToCurrent},
-            {symbol:'5', cols:1, action: this.addToCurrent},
-            {symbol:'6', cols:1, action: this.addToCurrent},
-            {symbol:'-', cols:1, action: this.addToCurrent},
-            {symbol:'1', cols:1, action: this.addToCurrent},
-            {symbol:'2', cols:1, action: this.addToCurrent},
-            {symbol:'3', cols:1, action: this.addToCurrent},
-            {symbol:'+', cols:1, action: this.addToCurrent},
-            {symbol:'0', cols:2, action: this.addToCurrent},
-            {symbol:'.', cols:1, action: this.addToCurrent},
-            {symbol:'=', cols:1, action: this.addToCurrent}
-        ];
-
         return(
             <div>
             <button className = "calc-button">1</button>
-            
-            {buttons.map((btn, i)==>{
-                <Button symbol= {btn.symbol} cols = {btn.cols} action={(symbol)=> btn.action} />
-            })}
             </div>
 
-        );
+        )
     }
 }
 
